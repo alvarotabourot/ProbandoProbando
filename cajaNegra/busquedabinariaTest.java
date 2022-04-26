e62b67a3439931b3e1fab5e1ef4bdd32a4ffc973
Alvaro <taboure12@gmail.com>
2022-04-26 08:30:53 +0200

    Ejercicio, Probando.. Probando

---
+++ cajaNegra/busquedabinariaTest.java
@@ -0,0 +1,24 @@
+package proyectoo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class busquedabinariaTest {
+
+	@Test
+	public void ENCUENTRAtest( ) {
+		int[] v = {-1, 2, 4, 40, 71, 100};
+		int esperado = 5;
+		int resultado = busquedabinaria.buscab(v, 100);
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void NO_ENCUENTRAtest() {
+		int[] v = {-1, 2, 4, 40, 71, 100};
+		int esperado = -1;
+		int resultado = busquedabinaria.buscab(v, 111);
+		assertEquals(esperado, resultado);
+	}
+}
